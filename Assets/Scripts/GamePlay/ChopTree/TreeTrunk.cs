3b70ef6f86f81af2bb8a0fc799e214a7ee36e585
Lucas Tomé Avelino Câmara <lucas.tacamara@gmail.com>
2020-04-21 21:47:28 -0300

    Destroy trunk particle effect

--- Assets/Scripts/GamePlay/ChopTree/TreeTrunk.cs
+++ Assets/Scripts/GamePlay/ChopTree/TreeTrunk.cs
@@ -6,8 +6,11 @@ namespace GamePlay.ChopTree
 {
     public class TreeTrunk : MonoBehaviour
     {
+        [SerializeField]
+        private GameObject _destroyEffect;
         public void Chop()
         {
+            Destroy(Instantiate(_destroyEffect, transform.position + Vector3.up * 2, _destroyEffect.transform.rotation), 3);
             Destroy(gameObject);
         }
     }
